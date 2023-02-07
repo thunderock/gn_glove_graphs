52166290af766af5cccba5c0d85387725479c9a7
Ashutosh Tiwari <ashutiwa@iu.edu>
2023-02-07 10:45:57 -0500

    need to compare now

--- GloVe-1.2/debias.sh
+++ GloVe-1.2/debias.sh
@@ -4,40 +4,36 @@
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
 make
-# if [ ! -e text8 ]; then
-#   if hash wget 2>/dev/null; then
-#     wget http://mattmahoney.net/dc/text8.zip
-#   else
-#     curl -O http://mattmahoney.net/dc/text8.zip
-#   fi
-#   unzip text8.zip
-#   rm text8.zip
-# fi
-# declare -A arguments
-# arguments=([dim]=16 [para]=8)
-DIMENSION=16
-PARA=8
-CORPUS=text8
+if [ ! -e text8 ]; then
+  if hash wget 2>/dev/null; then
+    wget http://mattmahoney.net/dc/text8.zip
+  else
+    curl -O http://mattmahoney.net/dc/text8.zip
+  fi
+  unzip text8.zip
+  rm text8.zip
+fi
+DIMENSION=$1
+PARA=$2
+CORPUS=/bigtemp/cj7an/en_wiki_corpus
 #CORPUS=$2
-VOCAB_FILE=vocab16.txt
-COOCCURRENCE_FILE=cooccurrence"$DIMENSION".bin
-COOCCURRENCE_SHUF_FILE=cooccurrence"$DIMENSION".shuf.bin
+VOCAB_FILE=vocab"$1".txt
+COOCCURRENCE_FILE=cooccurrence"$1".bin
+COOCCURRENCE_SHUF_FILE=cooccurrence"$1".shuf.bin
 BUILDDIR=build
-SAVE_FILE=vectors"$DIMENTION"-"$PARA"
+SAVE_FILE=vectors"$1"-"$2"
 VERBOSE=2
-MEMORY=500.0
-VOCAB_MIN_COUNT=0
-VECTOR_SIZE=16
+MEMORY=100.0
+VOCAB_MIN_COUNT=50
+VECTOR_SIZE=$1
 MAX_ITER=100
 WINDOW_SIZE=10
 BINARY=2
 NUM_THREADS=8
 X_MAX=100
-VOCAB_HASH_FILE=hashdump"$DIMENTION".txt
-MALE_WORD_FILE=../wordlist/male_word_file_new.txt
-FEMALE_WORD_FILE=../wordlist/female_word_file_new.txt
-
-python ../scripts/random_walk.py -n 1000000 -o $CORPUS -d airport -w $FEMALE_WORD_FILE -m $MALE_WORD_FILE
+VOCAB_HASH_FILE=hashdump"$1".txt
+MALE_WORD_FILE=../wordlist/male_word_file.txt
+FEMALE_WORD_FILE=../wordlist/female_word_file.txt
 
 $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
 if [[ $? -eq 0 ]]
@@ -56,7 +52,7 @@ if [[ $? -eq 0 ]]
 #           elif [ "$1" = 'octave' ]; then
 #               octave < ./eval/octave/read_and_evaluate_octave.m 1>&2 
 #           else
-               # python2 eval/python/evaluate.py --vocab_file $VOCAB_FILE --vectors_file $SAVE_FILE".txt"
+               python2 eval/python/evaluate.py --vocab_file $VOCAB_FILE --vectors_file $SAVE_FILE".txt"
 #           fi
 #       fi
     fi
